1282b1b7367b4d4ab7fb259ec6c9733c35b93540
iqbolshoh <iilhomjonov777@gmail.com>
2025-03-10 10:51:52 +0500

    commti

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
-$router->get('/', function() {
+$router->get('/', function () {
     return 'Welcome to the homepage!';
 });
 
-$router->get('/about', function() {
+$router->get('/about', function () {
     return 'About us page';
 });
 
-$router->get('/new', function() {
-    return 'About us page';
+$router->get('/new', function () {
+    return 'New page';
 });
