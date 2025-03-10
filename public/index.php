007a923c60c5725566be87a5da296ebf072e9240
iqbolshoh <iilhomjonov777@gmail.com>
2025-03-10 10:03:50 +0500

    commit

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../app/Core/Router.php';
+require_once __DIR__ . '/../app/Core/Request.php';
+require_once __DIR__ . '/../app/Core/Response.php';
+
+use App\Core\Router;
+
+$router = new Router();
+
+$router->get('/', function () {
+    return "Salom!";
+});
+
+$router->get('/home', function () {
+    return "Bosh sahifa!";
+});
+
+
+$router->resolve();
