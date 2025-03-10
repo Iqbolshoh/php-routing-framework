2835bc9e1f2c14f86e94c71d88fbcaf3d027cf7b
iqbolshoh <iilhomjonov777@gmail.com>
2025-03-10 10:35:50 +0500

    commit

---
+++ routes/api.php
@@ -0,0 +1,5 @@
+<?php
+
+$router->get('/api/users', function () {
+    return json_encode(['user1', 'user2', 'user3']);
+});
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+$router->get('/', function () {
+    return 'Welcome to the homepage!';
+});
+
+$router->get('/about', function () {
+    return 'About us page';
+});
